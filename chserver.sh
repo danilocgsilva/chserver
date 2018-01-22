6dbd13d97e8d5021557069b9adc0cea3d0cf4ade
Danilo Silva <danilocgsilva@gmail.com>
2018-01-21 22:06:27 -0200

    Getting all variables

--- chserver.sh
+++ chserver.sh
@@ -3,6 +3,7 @@
 ## version
 VERSION="0.0.1"
 
+## Asks again, if no value was given
 chserver_asknonempty () {
     read -p "$1" $2
 
@@ -12,6 +13,7 @@ chserver_asknonempty () {
     fi
 }
 
+## Asks again, if the value given is not an valid system path file
 chserver_askfile () {
     chserver_asknonempty "$1" $2
     
@@ -21,14 +23,23 @@ chserver_askfile () {
     fi
 }
 
+## Main function
 chserver () {
-    #read -p "Provides the full path from sql script: " full_path_sql_script
-    #echo $full_path_sql_script
     chserver_askfile "Provides the full sql script: " full_path_sql_script
-    echo $full_path_sql_script
 
-    read -p "Provides the old server: " old_server
-    echo $old_server
+    chserver_asknonempty "Provides the old domain: " old_domain
+
+    chserver_asknonempty "Provides the new domain: " new_domain
+
+    chserver_asknonempty "Provides the new server: " old_server_path
+
+    chserver_asknonempty "Provides the new server: " new_server_path
+    
+    echo Full sql script: $full_path_sql_script
+    echo Old server: $old_domain
+    echo New server: $new_domain
+    echo Old server path: $old_server_path
+    echo New server path: $new_server_path
 }
 
 ## detect if being sourced and
